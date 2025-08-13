8895214a2ac899bcefd33ca6b793f571a24b37ee
Akash-dev-lab <111779287+Akash-dev-lab@users.noreply.github.com>
2025-08-13 16:58:55 -0500

    imageKit integrated

--- backend/routes/song.routes.js
+++ backend/routes/song.routes.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const multer = require('multer')
+const uploadFile = require("../services/storage.services")
 
 const upload = multer({storage: multer.memoryStorage()})
 
-router.post('/songs', upload.single("image"), (req, res) => {
+router.post('/songs', upload.single("image"), async (req, res) => {
     console.log(req.body)
     console.log(req.file)
+    const fileData = await uploadFile(req.file)
+    console.log(fileData)
     res.json({
         message: 'Song received',
         song: req.body
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,12 @@
+require('dotenv').config()
 const express = require('express');
-const dotenv = require('dotenv');
 const connectDB = require('./db/db');
 const songRoutes = require('./routes/song.routes')
-dotenv.config();
 connectDB();
-
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
-
 app.use('/', songRoutes)
 
 app.listen(process.env.PORT, () => {
